4843f6092cb49f44e24813d5e4cf3cda793a78b7
MUTHURAJ R <sathishraj146@gmail.com>
2025-07-29 15:05:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ echo "🔄 Killing existing Node.js processes..."
 pkill node || echo "No Node.js process found."
 
 echo "🚀 Starting app..."
-cd ~/$WORKSPACE || { echo "❌ Failed to cd into ~/myapp"; exit 1; }
 
+# Go to workspace if needed
+cd "$WORKSPACE" || { echo "❌ Failed to cd into $WORKSPACE"; exit 1; }
+
+# Start the Node app
 nohup node app.js > app.log 2>&1 &
 
 echo "✅ App deployed!"
-
