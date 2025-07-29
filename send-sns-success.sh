c462065aa8b36e827cc32124fca3e0701e20a74d
MUTHURAJ R <sathishraj146@gmail.com>
2025-07-29 15:19:49 +0530

    Update send-sns-success.sh

--- send-sns-success.sh
+++ send-sns-success.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Success: App deployed"
+aws sns publish \
+  --topic-arn arn:aws:sns:ap-south-1:616461148225:ci-cd-alerts \
+  --message "✅ Success: App deployed successfully." \
+  --region ap-south-1
