821e497cbbb2d49ec3276fa2a9586a9d0a72d719
MUTHURAJ R <sathishraj146@gmail.com>
2025-07-31 19:07:18 +0530

    Update send-sns-failure.sh

--- send-sns-failure.sh
+++ send-sns-failure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aws sns publish \
-  --topic-arn arn:aws:sns:ap-south-1:616461148225:ci-cd-alerts \
+  --topic-arn arn:aws:sns:ap-south-1:idsns:ci-cd-alerts \
   --message "❌ Failure: Deployment failed." \
   --region ap-south-1
