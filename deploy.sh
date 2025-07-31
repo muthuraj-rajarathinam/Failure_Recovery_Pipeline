d1f8ebe31a916b496f422f45a977bf8f93bc8a39
MUTHURAJ R <sathishraj146@gmail.com>
2025-07-31 19:29:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "🚀 Starting app..."
 # Go to workspace if needed
 cd "$WORKSPACE" || { echo "❌ Failed to cd into $WORKSPACE"; exit 1; }
 
-# Start the Node app
+# Start the Node app in background and store the log
 nohup node app.js > app.log 2>&1 &
 
 echo "✅ App deployed!"
