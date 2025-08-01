03cc362a8f70096b4f98d23af04c7400921ee061
MUTHURAJ R <sathishraj146@gmail.com>
2025-08-01 18:25:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+APP_DIR="/home/ec2-user/myapp"
+
 echo "🔄 Killing existing Node.js processes..."
 pkill node || echo "No Node.js process found."
 
+echo "Deploying new version..."
+
+# Remove old app & deploy new one
+rm -rf "$APP_DIR"
+mkdir -p "$APP_DIR"
+cp -r $WORKSPACE/* "$APP_DIR"
+
+cd "$APP_DIR" || { echo "❌ Failed to cd into $APP_DIR"; exit 1; }
+
+npm install
+
 echo "🚀 Starting app..."
-
-# Go to workspace if needed
-cd "$WORKSPACE" || { echo "❌ Failed to cd into $WORKSPACE"; exit 1; }
-
-# Start the Node app in background and store the log
 nohup node app.js > app.log 2>&1 &
 
 echo "✅ App deployed!"
